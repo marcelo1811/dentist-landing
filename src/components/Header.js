c3d3264ccc3632a48e7ec77198cd5a40173be89c
marcelo1811 <miyachimarcelo@gmail.com>
2021-03-01 22:44:52 -0300

    feat: add header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import styles from '../styles/components/Header.module.css'
+
+export function Header() {
+  return (
+    <div className={styles.HeaderContainer}>
+      <h1>Logo</h1>
+      <div className={styles.LinksContainer}>
+        <div>
+          <a href=''>About</a>
+          <a href=''>Department</a>
+          <a href=''>Home</a>
+          <a href=''>Service</a>
+          <a href=''>Gallery</a>
+          <a href=''>Price</a>
+          <a href=''>Blog</a>
+          <a href=''>Contact</a>
+        </div>
+        <a href=''>Appointment</a>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Header } from '../components/Header'
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div>olá</div>
+    <div>
+      <Header />
+    </div>
   )
 }
